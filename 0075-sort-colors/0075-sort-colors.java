6b593a4878d390d5b75fc5a52b56af7a99e05cdb
Srinithi Veerapandiyan <srinithiveerapandiyan@gmail.com>
2025-04-06 12:51:40 +0530

    Time: 15 ms (3.37%), Space: 44.8 MB (5.09%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Arrays;
+
+class Solution {
+    public void sortColors(int[] nums) {
+       Arrays.sort(nums);
+       for(int i=0;i<nums.length;i++){
+        System.out.print(Arrays.toString(nums));
+       }
+    }
+}
